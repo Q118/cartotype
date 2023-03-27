094d5ae6e78c7bb7bb3a85d4b7fd43da9f49b0f1
shelby.rothman <shelby.rothman@aderant.com>
2023-03-27 13:40:34 -0700

    hardcoded add/remove display

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,7 +1,9 @@
+import { Button } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 import { formatCurrency } from '../utilities/formatCurrency';
 
-const TITLE_CLASSES:string = 'd-flex justify-content-between align-items-baseline mb-4';
+const TITLE_CLASSES: string = 'd-flex justify-content-between align-items-baseline mb-4';
+const INNER_BTN_CLASSES: string = 'd-flex align-items-center flex-column';
 
 type StoreItemProps = {
     id: number;
@@ -11,9 +13,13 @@ type StoreItemProps = {
 };
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+
+
+    const quantity: number = 0;
+
     return (
         <>
-            <Card>
+            <Card className='h-100'>
                 <Card.Img
                     variant="top"
                     src={imgUrl}
@@ -25,7 +31,25 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
                         <span className='fs-2'>{name}</span>
                         <span className='ms-2'>{formatCurrency(price)}</span>
                     </Card.Title>
-
+                    {/* mt-auto to fill all aviailable  space */}
+                    <div className='mt-auto'>
+                        {quantity === 0 ? (
+                            <Button className='w-100'>
+                                + Add To Cart
+                            </Button>
+                        ) : (
+                            <div className={INNER_BTN_CLASSES} style={{ gap: ".5rem" }}>
+                                <div className='d-flex align-items-center justify-content-center' style={{ gap: ".5rem" }}>
+                                    <Button>-</Button>
+                                    <div>
+                                        <span className='fs-3'>{quantity}</span> in cart
+                                    </div>
+                                    <Button>+</Button>
+                                </div>
+                                <Button variant="danger" size='sm'>Remove</Button>
+                            </div>
+                        )}
+                    </div>
 
                 </Card.Body>
             </Card>
