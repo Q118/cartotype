34580a4159459c10c392ac9c7483220958e69940
shelby.rothman <shelby.rothman@aderant.com>
2023-03-27 14:25:37 -0700

    offCanvas layouts

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,4 +1,5 @@
 import { ReactNode, createContext, useContext, useState } from 'react';
+import { ShoppingCart } from '../components/ShoppingCart';
 
 
 type ShoppingCartProviderProps = {
@@ -92,6 +93,7 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
             closeCart,
         }}>
             {children}
+            <ShoppingCart isOpen={isOpen} />
         </ShoppingCartContext.Provider>
     );
 }
