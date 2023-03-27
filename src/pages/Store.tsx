8455c56139828e41a81b7d626a8c6e6337849413
shelby.rothman <shelby.rothman@aderant.com>
2023-03-27 12:49:37 -0700

    iniital mapping of store values

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,8 +1,19 @@
+import storeItems from '../data/items.json';
+import { Col, Row } from 'react-bootstrap';
+
+
 function Store() {
     return (
-        <h1>
-            Store
-        </h1>
+        <>
+            <h1>
+                Store
+            </h1>
+            <Row>
+                {storeItems.map((item) => (
+                    <Col>{JSON.stringify(item)}</Col>
+                ))}
+            </Row>
+        </>
     );
 }
 
