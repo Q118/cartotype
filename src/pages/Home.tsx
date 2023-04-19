46d0d6059ddbcdb4807cb90d00c625b2d13c9fd7
shelby.rothman <shelby.rothman@aderant.com>
2023-04-19 14:15:32 -0700

    styling Home links

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,15 @@ import { BsFillCameraFill } from 'react-icons/bs';
 import { GiStabbedNote } from 'react-icons/gi';
 import { useTheme } from '../context/ThemeContext';
 
+const h5_spanStyle = {
+    marginTop: "1rem",
+    display: "flex",
+    gap: ".5rem",
+    justifyContent: "flex-end",
+};
+
+
+
 function Home() {
 
     const { currentTheme } = useTheme();
@@ -12,33 +21,19 @@ function Home() {
 
     return (
         <>
-            <h1>
-                Home
-            </h1>
+            <h1>Home</h1>
             <div
                 className={`shadow p-3 mb-5 rounded ${DIV_CLASS}`}
                 style={{ position: "absolute" }}>
-                <h5 style={{
-                    marginTop: "1rem",
-                    display: "flex",
-                    gap: ".5rem",
-                    justifyContent: "flex-end",
-                }}>
+                <h5 style={h5_spanStyle}>
                     <BsFillCameraFill />
-                    All Photos By <a className="style-link" href="https://unsplash.com/?utm_source=cartotype&utm_medium=referral" target="_blank">Unsplash</a>
+                    All Photos By <a className={`style-link link-${currentTheme}`} href="https://unsplash.com/?utm_source=cartotype&utm_medium=referral" target="_blank">Unsplash</a>
                 </h5>
                 <h3>Welcome to Cartotype; research, shop, sell, & more!</h3>
-
-                <p>Vist the <Link className="style-link" to="/store">store</Link> to buy items.</p>
-
-                <p>Visit the <Link to="/admin" className="style-link">admin</Link> page to add or edit items in your vendor shop.</p>
-                <p>And visit the <Link to="/notes" className="style-link">notes</Link> page to research the store products. There you can add your own notes to the community research pool.</p>
-                <span style={{
-                    marginTop: "1rem",
-                    display: "flex",
-                    gap: ".5rem",
-                    justifyContent: "flex-end",
-                }}>
+                <p>Vist the <Link className={`style-link link-${currentTheme}`} to="/store">store</Link> to buy items.</p>
+                <p>Visit the <Link to="/admin" className={`style-link link-${currentTheme}`}>admin</Link> page to add or edit items in your vendor shop.</p>
+                <p>And visit the <Link to="/notes" className={`style-link link-${currentTheme}`}>notes</Link> page to research the store products. There you can add your own notes to the community research pool.</p>
+                <span style={ h5_spanStyle}>
                     <small>- notes are markdown supported text that review product information <br />
                         - you can also view the notes per product directly from the store. Look for the <GiStabbedNote size={35} /> on a store item.</small>
                 </span>
