b520b8f8b8d1472da68818d5fee364c434590f43
shelby.rothman <shelby.rothman@aderant.com>
2023-06-09 15:51:09 -0700

    reerb

--- src/components/form/NoteListGroupFormBit.tsx
+++ src/components/form/NoteListGroupFormBit.tsx
@@ -1,30 +1,54 @@
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Form from 'react-bootstrap/Form';
 import { RawNote, RawNotedata } from '../../types';
 
 type NoteListGroupFormBitProps = {
 
-    currentNotesForItem: RawNote[];
-    onAddNote: (note: RawNotedata) => void;
-    onRemoveNote: (note: RawNotedata) => void;
+    // currentNotesForItem: RawNote[]|null;
+    notes: RawNote[] | null;
+    /** fire when a new selected is made */
+    onAddNote: (noteId: string) => void;
+    // we get it added within the updateFields function
+    /** fire when a selected is removed */
+    // onRemoveNote: (note: RawNotedata) => void;
     availableNotes: RawNote[];
     // ? tags
-}
+};
+
+const isEven = (num: number) => num % 2 === 0;
+
+export function NoteListGroupFormBit({
+    notes: currentNotesForItem = [],
+    availableNotes,
+    onAddNote
+}: NoteListGroupFormBitProps) {
+
+    // render out list for each note and theyll each have checkbox to nselect them
+//** oooorrrrr or maybe like a Small version of the noteList... llike query and seelct and find insidethe square
 
 
-export function NoteListGroupFormBit() {
+    const isAttached = (id: string) => currentNotesForItem && currentNotesForItem.find(note => note.id === id) ? true : false;
 
+    const renderNoteSelection = availableNotes.map((note: RawNote, index: number) => (
+        <ListGroup.Item className={`listItem-associatedNotes${isEven(index) ? '-alt' : ''}`} key={uuidv4()}>
+            <Form.Check
+                // type="switch"
+                type="checkbox"
+                id={`switch-note-${note.id}`}
+                label={note.title}
+                // checked={isAttached(note.id)}
+                onChange={() => onAddNote(note.id)}
+            />
+        </ListGroup.Item>
+    ));
 
 
     return (
         <>
             <ListGroup className="listGroup-associatedNotes">
-                <ListGroup.Item className="listItem-associatedNotes">note 1</ListGroup.Item>
-                <ListGroup.Item className="listItem-associatedNotes-alt">fmds,mf,.ds</ListGroup.Item>
-                <ListGroup.Item className="listItem-associatedNotes">note 2</ListGroup.Item>
-                <ListGroup.Item className="listItem-associatedNotes-alt">note dmsadka</ListGroup.Item>
-                <ListGroup.Item className="listItem-associatedNotes">ghfjdhdsk</ListGroup.Item>
-                <ListGroup.Item className="listItem-associatedNotes-alt">koekrl;ew</ListGroup.Item>
+                {renderNoteSelection}
             </ListGroup>
         </>
     )
