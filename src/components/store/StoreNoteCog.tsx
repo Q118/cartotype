2fb7c38bdff6a7f7b10c07a0ba8a80d0109b68a1
shelby.rothman <shelby.rothman@aderant.com>
2023-06-20 15:30:31 -0700

    cleanup

--- src/components/store/StoreNoteCog.tsx
+++ src/components/store/StoreNoteCog.tsx
@@ -1,9 +1,8 @@
 /** 
  * overlayed list of notes associated with the store item
 */
-import { MouseEvent, useRef, useState, useEffect } from 'react';
+import { useRef, useState } from 'react';
 import Overlay from 'react-bootstrap/Overlay';
-import ListGroup from 'react-bootstrap/ListGroup';
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 import remarkGfm from 'remark-gfm';
 import { GiStabbedNote } from 'react-icons/gi';
@@ -14,54 +13,37 @@ type StoreNoteCogProps = {
     storeItem_id: string;
 }
 
-const isEven = (num: number) => num % 2 === 0;
+
+// TODO a link to add a note maybe
 
 export function StoreNoteCog({ storeItem_id }: StoreNoteCogProps) {
     const targetRef = useRef(null);
-    // const [ showLocalModal, setShowLocalModal ] = useState(false);
-    // const [ modal_noteId, setModal_noteId ] = useState<string>('');
     const [ showOverlay, setShowOverlay ] = useState(false);
     const { getStoreItemById, availableNotes } = useShoppingCart();
 
     const getNoteFromAvailableNotes = (id: string) => availableNotes.find(note => note.id === id);
 
-    function handleOutsideClick(e: MouseEvent): void {
-        const mouseLocation = e.target;
-        // if it's not in the overlay or inside a card, then close it
-        if (mouseLocation !== targetRef.current) setShowOverlay(false);
-        return;
-    }
-
-
-
-
+    const returnNoNotes = () => <div className='storeTags-listGroup-card' style={{ padding: '5px' }}>
+        No notes for this item...<br />Go make some!
+    </div>;
 
     const renderListItems = (id: string) => {
         const storeItem = getStoreItemById(id);
-        // console.log('inside renderListItems', storeItem);
+        if (!storeItem.notes) return returnNoNotes();
         const { notes: noteId_list } = storeItem;
-        if (!noteId_list) return <ListGroup.Item className="storeTags-listGroup-card-list-item">0</ListGroup.Item>;
-
         return noteId_list.map((note_id: string, i: number) => {
             const note = getNoteFromAvailableNotes(note_id);
-            console.log('inside renderListItems', note);
-
-            if (!note) return <ListGroup.Item className="storeTags-listGroup-card-list-item">0</ListGroup.Item>;
-            return (
-                <div>
-                    <Accordian.Item key={note_id}
-                        className="note-accordian-item"
-                        // className={`note-accordian-item ${isEven(+i) ? 'alt-listItem' : ''}`}
-                        id={note_id} eventKey={i + ''}>
-                        <Accordian.Header className='note-accordian-header'>
-                            {note.title}
-                        </Accordian.Header>
-                        <Accordian.Body>
-                            <ReactMarkdown children={note.markdown} remarkPlugins={[ remarkGfm ]} />
-                        </Accordian.Body>
-                    </Accordian.Item>
-                </div>
-            )
+            if (!note) return returnNoNotes();
+            return <div>
+                <Accordian.Item key={note_id} className="note-accordian-item" id={note_id} eventKey={i + ''}>
+                    <Accordian.Header className='note-accordian-header'>
+                        {note.title}
+                    </Accordian.Header>
+                    <Accordian.Body>
+                        <ReactMarkdown children={note.markdown} remarkPlugins={[ remarkGfm ]} />
+                    </Accordian.Body>
+                </Accordian.Item>
+            </div>
         });
     }
 
@@ -80,7 +62,7 @@ export function StoreNoteCog({ storeItem_id }: StoreNoteCogProps) {
             </Overlay>
             {/* The trigger */}
             <button onClick={() => setShowOverlay(true)} ref={targetRef}
-                className="stabbed-note-btn" title='view notes on this item'>
+                className="stabbed-note-btn" title='preview notes on this item'>
                 <GiStabbedNote size={35} />
             </button>
         </>
